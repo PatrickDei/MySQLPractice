5850c256dee1713f9834112c014715c80e064e81
Patrick Deisinger <patrick.deis@gmail.com>
2021-06-10 20:36:32 +0200

    Plain Java -> Spring

---
+++ sqlprac/src/main/java/hr/deisinger/sqlprac/SqlpracApplication.java
@@ -0,0 +1,13 @@
+package hr.deisinger.sqlprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlpracApplication.class, args);
+	}
+
+}
